e3b1796d06c0fa5ec6e19bc0f91d26f185835e09
Zaw Ye Htet (DAT) <zawyehtet7928@gmail.com>
2023-11-01 15:47:19 +0630

    Fifth commit.

--- test.php
+++ test.php
@@ -1,10 +1,30 @@
 <?php
 
-$result = "";
-$name = $result;
 
-echo $name; // Alice
+class Math
+{
+
+    private $PI = 3.14;
+
+    public function __get($name)
+    {
+        return $this->PI;
+
+    }
+    public function __set($name, $value)
+    {
+        $this->PI = $value;
+    }
+
+}
+
+$obj = new Math;
 
 
 
+echo $obj->PI; // Cannot access PI
+echo "<br>";
+$obj->PI = 12; // Cannot set PI with 3.142
+echo "<br>";
+echo $obj->PI; // Cannot access PI
 
