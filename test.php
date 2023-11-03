4b49951fd66e91042733ba42ab315f4145888246
Zaw Ye Htet (DAT) <zawyehtet7928@gmail.com>
2023-11-03 16:48:11 +0630

    Sixth commit.

--- test.php
+++ test.php
@@ -1,30 +1,52 @@
 <?php
-
-
-class Math
+interface Log
 {
-
-    private $PI = 3.14;
-
-    public function __get($name)
+    public function write();
+}
+class Text implements Log
+{
+    public function write()
     {
-        return $this->PI;
-
+        echo "Saving to text file";
     }
-    public function __set($name, $value)
+}
+class Memory implements Log
+{
+    public function write()
     {
-        $this->PI = $value;
+        echo "Saving on memory";
     }
-
 }
 
-$obj = new Math;
+class Services
+{
+    public $container = [];
+    public function register($name, $class)
+    {
+        $this->container[$name] = $class;
+    }
+}
+$services = new Services;
+$services->register("text", Text::class);
+$services->register("memory", Memory::class);
 
+class Provider
+{
+    public $services;
+    public function __construct($services)
+    {
+        $this->services = $services->container;
+    }
+    public function make($service)
+    {
+        if (isset($this->services[$service]))
+            return new $this->services[$service];
+        // else Error: Service doesn't exist
+    }
+}
 
-
-echo $obj->PI; // Cannot access PI
-echo "<br>";
-$obj->PI = 12; // Cannot set PI with 3.142
-echo "<br>";
-echo $obj->PI; // Cannot access PI
-
+$provider = new Provider($services);
+$log = $provider->make("text");
+$log->write(); // Saving to text file
+$log = $provider->make("memory");
+$log->write(); // Saving on memory
