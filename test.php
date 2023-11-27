543d1056c3944a8668c1ca56475af74fc7c74001
Zaw Ye Htet <zawyehtet7928@gmail.com>
2023-11-27 20:24:39 +0630

    pg.611

--- test.php
+++ test.php
@@ -1,3 +1,20 @@
-
 <?php
-print_r( $_SERVER );
+
+$db = new PDO(
+    'mysql:dbhost=localhost;dbname=project_01',
+    'root',
+    '',
+    [
+        PDO::ATTR_ERRMODE => PDO::ERRMODE_WARNING,
+        PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_OBJ,
+    ]
+
+);
+
+$sql = "DELETE FROM roles WHERE id > 3";
+
+$statement = $db->prepare($sql);
+
+$statement->execute();
+
+echo $statement->rowCount();
